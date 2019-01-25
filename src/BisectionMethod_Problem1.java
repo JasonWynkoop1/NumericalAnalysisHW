dec68b69bbd92cd0db5a6a8c7fa089de9a7d6a7a
Jason <jasonwynkoop1@yahoo.com>
2019-01-25 18:36:50 -0500

    Changed some things with init input

--- src/BisectionMethod_Problem1.java
+++ src/BisectionMethod_Problem1.java
@@ -10,21 +10,24 @@ class BisectionMethod_Problem1 {
 
     public static void main(String[] args){
 
-        if(args.length == 0){
-            System.out.println("NEED INTERVAL AND ACCURACY AS INPUTS");
-            System.exit(0);
+        if (args.length < 3) {
+            double initial_a = 0;
+            double initial_b = 1;
+            double accuracy = .000001;
+            System.out.println("\nUSING DEFAULT INTERVAL OF [" + initial_a + ", " + initial_b + "]");
+
+            System.out.println("The approximate solution is: "
+                    + bisection(initial_a, initial_b, accuracy)
+                    + " after " + i + " iterations.");
+        } else {
+
+            System.out.println("The approximate solution is: "
+                    + bisection(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]))
+                    + " after " + i + " iterations.");
         }
-
-        System.out.println("Getting initial interval using the given interval of ["
-                + args[0] + ", " + args[1] + "].\n");
-
-        System.out.println("The approximate solution is: "
-                + bisection(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2])) + " after " + i + " iterations.");
     }
 
     private static double bisection(double a, double b, double c){
-
-        double TOL = c;
 
         double FA = Math.exp(a) - Math.pow(a, 2) + (3 * a) - 2;
         int maxRuns = 100;
@@ -32,7 +35,7 @@ class BisectionMethod_Problem1 {
             double p = a + (b - a) / 2;
 
             double FP = Math.exp(p) - Math.pow(p, 2) + (3 * p) - 2;
-            if(FP == 0 || (Math.abs((b-a)) <= TOL)){
+            if (FP == 0 || (Math.abs((b - a)) <= c)) {
                 return p;
             }
 
--- src/BisectionMethod_Problem2.java
+++ src/BisectionMethod_Problem2.java
@@ -10,16 +10,27 @@ public class BisectionMethod_Problem2 {
 
     public static void main(String[] args){
 
-        if(args.length == 0){
+        /*if(args.length == 0){
             System.out.println("NEED INTERVAL AND ACCURACY AS INPUTS");
             System.exit(0);
+         } */
+
+        if (args.length < 3) {
+            double initial_a = 1.7;
+            double initial_b = 1.75;
+            double accuracy = .000001;
+            System.out.println("USING DEFAULT INTERVAL OF [" + initial_a + ", " + initial_b + "]");
+            System.out.println("Getting initial interval using the given interval of ["
+                    + initial_a + ", " + initial_b + "].\n");
+
+            System.out.println("The approximate solution is: "
+                    + bisection(initial_a, initial_b, accuracy)
+                    + " after " + i + " iterations.");
+        } else {
+            System.out.println("The approximate solution is: "
+                    + bisection(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]))
+                    + " after " + i + " iterations.");
         }
-
-        System.out.println("Getting initial interval using the given interval of ["
-                + args[0] + ", " + args[1] + "].\n");
-
-        System.out.println("The approximate solution is: "
-                + bisection(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2])) + " after " + i + " iterations.");
     }
 
     /**
@@ -31,7 +42,6 @@ public class BisectionMethod_Problem2 {
      */
     private static double bisection(double a, double b, double c){
         //setting tolerance
-        double TOL = c;
         //getting sign for a
         double FA = Math.pow(a, 2) - 3;
 
@@ -45,7 +55,7 @@ public class BisectionMethod_Problem2 {
 
             double FP = Math.pow(p, 2) - 3;
 
-            if(FP == 0 || (Math.abs((b-a)) <= TOL)){
+            if ((FP == 0) || (Math.abs((b - a)) <= c)) {
                 return p;
             }
 
