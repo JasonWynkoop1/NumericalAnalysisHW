8bde59bbb3e5dd8747dd3ff245479e1ce64f9ecf
Jason <jasonwynkoop1@yahoo.com>
2019-02-06 20:34:31 -0500

    Finished

--- src/NewtonsMethod_Problem2.java
+++ src/NewtonsMethod_Problem2.java
@@ -1,3 +1,9 @@
+/**
+ * Use Newton's Method to approximate 10^-4, the value of x that produces the point on the
+ * graph y = 1/x that is closest to (2, 1).
+ * <p>
+ * Problem 2 / Exercise 20 / Page 76
+ */
 public class NewtonsMethod_Problem2 {
 
     private static int i = 1;
@@ -5,17 +11,23 @@ public class NewtonsMethod_Problem2 {
     private static int maxRuns = 200;
 
     public static void main(String[] args) {
-
-        System.out.println("(" + newtonsMethod() + ", " + 1 / newtonsMethod() + ")");
+        System.out.println("VALUE OF X: " + newtonsMethod());
     }
 
+
+    /**
+     * Newton's Method
+     * @param accuracy - tolerance
+     * @return - zero
+     */
     private static double newtonsMethod() {
         while (i < maxRuns) {
-            double p = p0 + (function(p0) / functionPrime(p0));
+            double p = p0 - (function(p0) / functionPrime(p0));
             if (Math.abs(p - p0) < Math.pow(10, -4)) {
+                System.out.println("STEP: " + i + " (" + p + ", " + 1 / p + ")");
                 return p;
             }
-            System.out.println("STEP: " + i + "(" + p + ", " + 1 / p + ")");
+            System.out.println("STEP: " + i + " (" + p + ", " + 1 / p + ")");
             p0 = p;
             i++;
         }
